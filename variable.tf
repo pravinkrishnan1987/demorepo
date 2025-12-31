5b710b88de2b3dae850e3b8f213b83032648e77b
pravinkrishnan1987 <pravinkrishnan.1987@gmail.com>
2025-12-31 18:08:35 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,20 @@
+# Server Module Variables
+variables "instance_name" {
+ description = "Instance Name"
+ default = ""
+}
+
+variable "ami" {
+  description = "Gowith you ami ID"
+  default = "ami-xxx"
+}
+
+variable "Instance_type' {
+  description = "Instance Type"
+}
+
+#variable "key" {
+  #description ="Go wuth you aws ketpair"
+  #default = "ami-xxxx"
+} 
+
